602cf6f77878d6426ddce6fa1b38f413e9a992ee
woddu <fwoddu@yahoo.com>
2025-08-02 12:01:04 +0800

    fixed exposed secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,19 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNGHfd-y95qbzwOw8Mu79LPFuYAXfBd_E",
-  authDomain: "vue-firebase-test-3b12a.firebaseapp.com",
-  projectId: "vue-firebase-test-3b12a",
-  storageBucket: "vue-firebase-test-3b12a.firebasestorage.app",
-  messagingSenderId: "748264823489",
-  appId: "1:748264823489:web:5094843349e226b00b1240",
-  measurementId: "G-5M24D5MEQK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
