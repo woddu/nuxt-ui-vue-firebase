9a3a854818967729d4b31e193dba512a90a1cc0b
woddu <fwoddu@yahoo.com>
2025-08-10 23:57:00 +0800

    Designing Dashboard...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,5 +8,13 @@ import AuthenticatedLayout from '@/components/layouts/AuthenticatedLayout.vue';
         <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted mb-4">Dashboard</h1>
 
         <USeparator class="mt-4 mb-8 px-2" color="primary" size="lg" />
+
+        <UCard class="p-4">
+            <template #header>
+                <h2 class="text-xl font-bold">Welcome to your dashboard</h2>
+            </template>
+            <p class="mt-2">Here you can find an overview of your account and activities.</p>
+        </UCard>
+        
     </AuthenticatedLayout>
 </template>
