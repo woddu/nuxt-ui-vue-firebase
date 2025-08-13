30e5abad6873b828f2cb553b6ba0d48b2fcb7a98
woddu <fwoddu@yahoo.com>
2025-08-13 18:27:07 +0800

    added the omponenets page header title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import AuthenticatedLayout from '@/components/layouts/AuthenticatedLayout.vue';
 
 <template>
     <AuthenticatedLayout>
-        <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted mb-4">Dashboard</h1>
+        <PageHeaderTitle title="Dashboard" />
 
         <USeparator class="mt-4 mb-8 px-2" color="primary" size="lg" />
 
