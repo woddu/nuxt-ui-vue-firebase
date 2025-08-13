77e519cd7a7c7f868ac9016f8a4839b5814d257c
woddu <fwoddu@yahoo.com>
2025-08-13 18:25:16 +0800

    turned the page header title intoa component

---
+++ src/components/PageHeaderTitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted mb-4">
+        {{ title }}
+    </h1>
+</template>
