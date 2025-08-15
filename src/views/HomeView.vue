9fb5cf7c7e149736259c8613394777ed9702f9df
woddu <fwoddu@yahoo.com>
2025-08-15 23:48:05 +0800

    workingon cards in dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/components/layouts/AuthenticatedLayout.vue';
+import useStudentsCount from '@/composables/useStudentsCount';
+import { onMounted, ref } from 'vue';
 
+
+const pending = ref(true);
+const count = ref(0);
+
+onMounted(async () => {
+    const { pending: newPending, count: newCount } = await useStudentsCount();
+    pending.value = newPending.value;
+    count.value = newCount.value;
+});
 </script>
 
 <template>
@@ -9,12 +20,22 @@ import AuthenticatedLayout from '@/components/layouts/AuthenticatedLayout.vue';
 
         <USeparator class="mt-4 mb-8 px-2" color="primary" size="lg" />
 
-        <UCard class="p-4">
-            <template #header>
-                <h2 class="text-xl font-bold">Welcome to your dashboard</h2>
-            </template>
-            <p class="mt-2">Here you can find an overview of your account and activities.</p>
-        </UCard>
+        <!-- grid of cards for dashboard -->
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <UCard class="px-4 pt-4 pb-2" variant="soft">
+                <div v-if="pending">
+                    <USkeleton />
+                </div>
+                <div class="flex justify-between gap-4">
+                    <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
+                        Students 
+                    </h2>
+                    <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
+                            {{ count }}
+                    </h2>
+                </div>            
+            </UCard>
+        </div>            
         
     </AuthenticatedLayout>
 </template>
