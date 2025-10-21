48ffce0fab9c828e3c37c3cfbfc0e8808b8951c3
woddu <fwoddu@yahoo.com>
2025-10-21 21:21:16 +0800

    added section service for section CRUD(mutate)

---
+++ src/services/sectionService.ts
@@ -0,0 +1,24 @@
+import { db } from '@/firebase'
+import { collection, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore'
+import type { Section } from '@/interfaces'
+
+export const sectionsRef = collection(db, "sections");
+
+export async function addSection(section: Section) {
+  const docRef = doc(sectionsRef)
+  await setDoc(docRef, { ...section, id: docRef.id })
+  return docRef.id
+}
+
+export async function updateSection(section: Section) {
+  if (!section.id) {
+    throw new Error('Section ID is required for update.')
+  }
+  const { id, ...data } = section
+  const docRef = doc(sectionsRef, id)
+  await updateDoc(docRef, data) // safer update
+}
+
+export async function deleteSection(id: string) {
+  await deleteDoc(doc(sectionsRef, id))
+}
