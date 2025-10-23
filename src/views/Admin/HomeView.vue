d75db690ba6280300ec982732047e29fa3069c47
woddu <fwoddu@yahoo.com>
2025-10-23 11:46:52 +0800

    added teacher count

--- src/views/Admin/HomeView.vue
+++ src/views/Admin/HomeView.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/components/layouts/AuthenticatedLayout.vue';
 import { getStudentCount } from '@/composables/useStudents';
+import { getTeachersCount } from '@/composables/useTeachers';
 import { onMounted, ref } from 'vue';
 
 
 const pending = ref(true);
-const count = ref(0);
+
+const studentCount = ref(0);
+
+const teacherCount = ref(0);
 
 onMounted(async () => {
-    try {
-    // all students
-    count.value = await getStudentCount()
-
-    // or for a specific section
-    // count.value = await getStudentCount('sec001')
-  } catch (err: any) {
-    // error.value = err.message ?? 'Failed to fetch count'
-  } finally {
-    pending.value = false
-  }
+    try {        
+        studentCount.value = await getStudentCount();
+        
+        teacherCount.value = await getTeachersCount();
+    } catch (err: any) {
+        // error.value = err.message ?? 'Failed to fetch count'
+    } finally {
+        pending.value = false
+    }
 });
 </script>
 
@@ -27,28 +29,28 @@ onMounted(async () => {
         <!-- grid of cards for dashboard -->
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <UCard class="px-4 pt-4 pb-2" variant="soft">
-                <div v-if="pending || count === 0">
-                    <USkeleton />
-                </div>
                 <div class="flex justify-between gap-4">
                     <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
                         Students 
                     </h2>
-                    <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
-                            {{ count }}
+                    <div v-if="pending || studentCount === 0">
+                        <USkeleton />
+                    </div>
+                    <h2 v-else class="text-xl lg:text-2xl text-highlighted font-bold">
+                            {{ studentCount }}
                     </h2>
                 </div>            
             </UCard>
             <UCard class="px-4 pt-4 pb-2 col-span-2" variant="soft">
-                <div v-if="pending || count === 0">
-                    <USkeleton />
-                </div>
                 <div class="flex justify-between gap-4">
                     <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
                         Students
                     </h2>
-                    <h2 class="text-xl lg:text-2xl text-highlighted font-bold">
-                        {{ count }}
+                    <div v-if="pending || teacherCount === 0">
+                        <USkeleton />
+                    </div>
+                    <h2 v-else class="text-xl lg:text-2xl text-highlighted font-bold">
+                        {{ teacherCount }}
                     </h2>
                 </div>
             </UCard>
