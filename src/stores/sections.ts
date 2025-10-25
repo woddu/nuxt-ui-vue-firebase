43bf9844f2637e0187b32fdcbb4466bf6b7152a2
woddu <fwoddu@yahoo.com>
2025-10-25 21:39:05 +0800

    added a store for section for preventing re-reads

---
+++ src/stores/sections.ts
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+import { _RefFirestore, VueFirestoreQueryData } from 'vuefire'
+import { Section } from '@/interfaces'
+import { useSections } from '@/composables/useSections'
+
+export const useSectionStore = defineStore('sections', () => {
+    let sectionsCollection: _RefFirestore<VueFirestoreQueryData<Section>>;
+    let stopSections: (() => void) | null = null
+
+    function start() {
+        // bind only once
+        if (!stopSections) {
+            sectionsCollection = useSections()
+            stopSections = sectionsCollection.stop
+        }
+    }
+
+    function sections() {
+        if (sectionsCollection) {
+            return sectionsCollection;
+        } else {
+            start();
+            return sectionsCollection;
+        }
+    }
+
+    function stop() {
+        if (stopSections) {
+            stopSections()
+            stopSections = null
+        }
+    }
+
+    return { sections, start, stop }
+})
