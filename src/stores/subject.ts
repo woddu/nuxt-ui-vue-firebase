b11c5a49730e018108572e79a0fcb7d79a0b71f2
woddu <fwoddu@yahoo.com>
2025-10-25 21:38:53 +0800

    added a store for subject for preventing re-reads

---
+++ src/stores/subject.ts
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia'
+import { _RefFirestore, VueFirestoreQueryData } from 'vuefire'
+import { useSubjects } from '@/composables/useSubjects'
+import { Subject } from '@/interfaces'
+
+export const useSubjectStore = defineStore('subjects', () => {
+    let subjectsCollection: _RefFirestore<VueFirestoreQueryData<Subject>>;
+    let stopSubjects: (() => void) | null = null
+
+    function start() {
+        // bind only once
+        if (!stopSubjects) {
+            subjectsCollection = useSubjects()
+            stopSubjects = subjectsCollection.stop
+        }
+    }
+
+    function subjects() {
+        if (subjectsCollection) {
+            return subjectsCollection;
+        } else {
+            start();
+            return subjectsCollection;
+        }
+    }
+
+    function stop() {
+        if (stopSubjects) {
+            stopSubjects()
+            stopSubjects = null
+        }
+    }
+
+    return { subjects , start, stop }
+})
