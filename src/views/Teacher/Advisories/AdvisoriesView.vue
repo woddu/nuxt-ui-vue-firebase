6e81fef165089c5d185a9a3f3f57accdb8a5af40
woddu <fwoddu@yahoo.com>
2025-10-28 19:51:11 +0800

    moved to its own folder

---
+++ src/views/Teacher/Advisories/AdvisoriesView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+
+const isLoading = ref(false);
+
+</script>
+
+<!-- 
+    flow:
+        1. Show Advisories in a table with action: view Students, SOG
+        2. On click of view Students, navigate to Advisory Students view
+        3. In Advisory Students view, show list of students in a table under this advisory with actions to view details
+        4. On click of SOG, navigate to Advisory SOG view
+        5. In Advisory SOG view, show list of SOGs in a table under this advisory with actions to view grades
+
+-->
+
+<template>
+    <AuthenticatedLayout :progress="isLoading" title="Advisories">
+        <RouterView @loading="(value: boolean) => isLoading = value" />
+    </AuthenticatedLayout>
+</template>
+
