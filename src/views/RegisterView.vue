e0f54becfa47fa0e5c0da77f9627c18823126da6
woddu <fwoddu@yahoo.com>
2025-10-28 19:54:17 +0800

    changed the register logic, adds fields of a teacher in form, check if email exists, then adds a teacher if register is succesful

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,6 +4,8 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { reactive, ref } from 'vue';
 import type { FormError, FormSubmitEvent } from '@nuxt/ui'
 import { useRouter } from 'vue-router';
+import { addTeacher, getTeacherByEmail } from '@/services/teacherService';
+import { Teacher } from '@/interfaces';
 
 const auth = getAuth();
 
@@ -12,74 +14,116 @@ const router = useRouter();
 const isLoading = ref(false)
 
 const state = reactive({
-  email: '',
-  password: ''
+    username: '',
+    email: '',
+    lastName: '',
+    firstName: '',
+    password: ''
 })
 
 const validate = (state: any): FormError[] => {
-  const errors = []
-  if (!state.email) errors.push({ name: 'email', message: 'Required' })
-  if (!state.password) errors.push({ name: 'password', message: 'Required' })
-  return errors
+    const errors = []
+    if (!state.email) errors.push({ name: 'email', message: 'Required' })
+    if (!state.password) errors.push({ name: 'password', message: 'Required' })
+    return errors
 }
 
 const toast = useToast()
 
 async function onSubmit(event: FormSubmitEvent<typeof state>) {
     isLoading.value = true
-    await createUserWithEmailAndPassword(auth, state.email, state.password)
-        .then((data) =>{
-            // console.log('User registered successfully:', data.user);
-            // console.log(auth.currentUser);
-            toast.add({ title: 'Success', description: 'Registered successfully.', color: 'success' })
-            router.push('/');
-        })
-        .catch((error) => {
-            console.error('Error registering user:', error);
-            toast.add({ title: 'Error', description: error.message, color: 'error' });
-        }).finally(() => {
-            isLoading.value = false
+    const errors = validate(state);
+    if (errors.length) {
+        let errorMessage = '';
+        isLoading.value = false;
+        errors.forEach((error) => {
+            errorMessage += `${error.message}\n`;
         });
+        toast.add({ title: "Error", description: errorMessage, color: "error" });
+        return;
+    }
+    try {
+        // 2. Query Firestore for the user document
+        const snapshot = await getTeacherByEmail(state.email);
+
+        if (!snapshot.empty) {
+            toast.add({ title: "Error", description: "Email already in use.", color: "error" });
+            isLoading.value = false;
+            return;
+        }
+
+        const { user } = await createUserWithEmailAndPassword(auth, state.email, state.password)
+
+        if (!user) {
+            throw new Error('Failed to create user');
+        }
+
+        const newTeacher: Omit<Teacher, 'id'> = {
+            email: user.email ?? '',
+            firstName: state.firstName,
+            lastName: state.lastName,
+            username: state.username,
+            verified: false,
+            role: 'teacher'
+        }
+
+        addTeacher(newTeacher as Teacher)
+            .catch((error) => {
+                throw new Error('Failed to add teacher: ' + error.message);
+            })
+            .finally(() => {
+                isLoading.value = false;
+            })
+
+        toast.add({ title: 'Success', description: 'Registered successfully.', color: 'success' })
+        router.push('/Login');
+    } catch (error: any) {
+        toast.add({ title: 'Error', description: error.message, color: 'error' });
+    } finally {
+        isLoading.value = false
+    }
 }
 
 </script>
 
 <template>
     <GuestLayout :progress="isLoading">
-        <UCard class="max-w-md w-full p-6">
+        <UCard class="max-w-2xs sm:max-w-xs md:max-w-md w-full p-4 md:p-6" variant="soft">
             <template #header>
-                <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted">Register</h1>                
-            </template>
-            
-            <UForm :validate="validate" :state="state"  @submit="onSubmit">
-                <div class="flex flex-col gap-4 justify-center">
-                    <UFormField label="Email" class="w-full" size="xl">
-                        <UInput
-                          class="w-full"
-                          v-model="state.email"
-                          type="email"
-                          placeholder="Enter your email"
-                          required />
-                    </UFormField>
-                    <UFormField label="Password" class="w-full" size="xl">
-                        <UInput
-                          class="w-full"
-                          v-model="state.password"
-                          type="password"
-                          placeholder="Enter your password"
-                          required />
-                    </UFormField>
+                <div class="w-full flex justify-center items-center">
+                    <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted">Register</h1>
                 </div>
-                <UButton
-                  :disabled="isLoading"
-                  type="submit"
-                  label="Register"
-                  class="mt-4" 
-                  size="xl"/>
+            </template>
+
+            <UForm :validate="validate" :state="state" @submit="onSubmit">
+                <div class="flex flex-col gap-4 justify-center">
+                    <div class="flex flex-col sm:flex-row gap-4">
+                        <UFormField label="First Name" class="w-full" size="xl">
+                            <UInput class="w-full" v-model="state.firstName" type="text" placeholder="Enter your first name" required />
+                        </UFormField>
+                        <UFormField label="Last Name" class="w-full" size="xl">
+                            <UInput class="w-full" v-model="state.lastName" type="text" placeholder="Enter your last name" required />
+                        </UFormField>
+                    </div>
+                    <div class="flex flex-col sm:flex-row gap-4">
+                        <UFormField label="Username" class="w-full" size="xl">
+                            <UInput class="w-full" v-model="state.username" type="text" placeholder="Enter your username" required />
+                        </UFormField>
+                        <UFormField label="Email" class="w-full" size="xl">
+                            <UInput class="w-full" v-model="state.email" type="email" placeholder="Enter your email"
+                                required />
+                        </UFormField>
+                    </div>
+                    <UFormField label="Password" class="w-full" size="xl">
+                        <UInput class="w-full" v-model="state.password" type="password"
+                            placeholder="Enter your password" required />
+                    </UFormField>
+                    <UButton :disabled="isLoading" type="submit" label="Register" class="mt-4 justify-center items-center" size="xl" />
+                </div>
             </UForm>
             <template #footer>
                 <p class="text-center text-sm text-gray-500">
-                    Already have an account? 
+                    Already have an account?
                     <ULink to="/login" class="text-primary hover:underline">Login here</ULink>
                 </p>
             </template>
