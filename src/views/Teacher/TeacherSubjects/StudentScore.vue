41c43a4ecee0df5ba7d1818abda3a35cdf812d19
woddu <fwoddu@yahoo.com>
2025-11-14 23:08:06 +0800

    added view for scores of a student

---
+++ src/views/Teacher/TeacherSubjects/StudentScore.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useScoresByStudent } from '@/composables/useScores';
+import { useRouter } from 'vue-router';
+
+const emit = defineEmits<{
+    (e: 'loading', value: boolean): void
+}>()
+
+const router = useRouter();
+
+const studentScore = useScoresByStudent(router.currentRoute.value.params.id as string)
+
+</script>
+
+<template>
+
+    
+</template>
